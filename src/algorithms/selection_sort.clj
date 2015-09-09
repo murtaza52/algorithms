250fdd032a8da11449e1524d032ee245953e097d
Murtaza Husain <murtaza52@gmail.com>
2015-09-09 16:37:53 +0530

    all

--- src/algorithms/selection_sort.clj
+++ src/algorithms/selection_sort.clj
@@ -1,4 +1,5 @@
-(ns algorithms.selection-sort)
+(ns algorithms.selection-sort
+  (:require [clojure.tools.trace :refer [trace]]))
 
 ;; Create a way to retrieve all mins in O(n) time.
 (defn mins [coll]
@@ -9,7 +10,6 @@
               1 [min-coll (conj rest-coll val)]))
           [[(first coll)] []]
           (rest coll)))
-
 ;; (mins [3 1 1 2]) => [[1 1] [3 2]]
 
 (defn selection-sort [coll]
@@ -21,12 +21,16 @@
 
 (selection-sort [3 1 1 2 5 7 8 8 4 6])
 
-(reduce (fn[acc val]
-          (mins ))
-        []
-        [3 1 1 2])
-
 (def selection-sort
   (lazy-cat (mins coll)))
 
 ;; (reduce (fn[vall acc] ) [] [:a [:a]])
+
+
+(defn selection-sort
+  [input]
+  (let [ixs (vec (range (count input)))
+        min-key-from (fn [acc ix] (apply min-key acc (subvec ixs ix)))
+        swap (fn [coll i j] (assoc coll i (coll j) j (coll i)))]
+    (reduce
+     (fn [acc ix] (swap acc ix (min-key-from acc ix))) input ixs)))
